f37118a197c31e1f2c4a87c4a41bce00c152af42
Vitriani <vitririya3@gmail.com>
2025-03-14 08:31:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const qr_box = document.querySelector(".qr_box"),
+qrInput = qr_box.querySelector(".form input"),
+generateBtn = qr_box.querySelector(".form button"),
+qrImg = qr_box.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        qr_box.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        qr_box.classList.remove("active");
+        preValue = "";
+    }
+});
